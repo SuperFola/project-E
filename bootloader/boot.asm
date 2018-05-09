989a28e8a9447967c033f1755d14ade5a582ac3e
SuperFola <lexplt.dev@gmail.com>
2018-05-09 23:05:03 +0200

    boot.asm now compiling [16 bits]

--- bootloader/boot.asm
+++ bootloader/boot.asm
@@ -1,128 +1,128 @@
 bits 16
-org 0
+; org 0
 
 start:
-	jmp main
+    jmp main
 
 main:
-	cli              ; move registers for offset of BIOS 07C0h load point
-	mov ax, 07C0h    ; offset
-	mov ds, ax
-	mov es, ax
-	mov fs, ax
-	mov gs, ax
+    cli              ; move registers for offset of BIOS 07C0h load point
+    mov ax, 07C0h    ; offset
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
 
-	mov ax, 0x0000   ; init the stack
-	mov ss, ax
-	mov sp, 0xffff
-	sti
+    mov ax, 0x0000   ; init the stack
+    mov ss, ax
+    mov sp, 0xffff
+    sti
 
-	mov si, message
-	call proj_e_boot_print
-	call proj_e_boot_getkeypress
+    mov si, message
+    call proj_e_boot_print
+    call proj_e_boot_getkeypress
 
-	mov ax, 0x01       ; LBA number 1 for sector
-	mov cx, 0x02       ; read two sectors from the floppy disk
-	mov bx, 0x200
-	; call the read sectors function
-	call proj_e_boot_readsectors
+    mov ax, 0x01       ; LBA number 1 for sector
+    mov cx, 0x02       ; read two sectors from the floppy disk
+    mov bx, 0x200
+    ; call the read sectors function
+    call proj_e_boot_readsectors
 
-	; address ES offset BX returned from read sectors (call kernel)
-	jmp 0x7e0:0
+    ; address ES offset BX returned from read sectors (call kernel)
+    jmp 0x7e0:0
 
 data:
-	; strings
-	message    db 'Hello World! Press any key to load the kernel ', 13, 10, 0
-	; parameters
-	sectors_per_track  dw  18
-	heads_per_track    dw   2
-	bytes_per_sector   dw 512
-	drive_number       dw   0
-	; variables
-	abs_sector dw 0x00
-	abs_head   dw 0x00
-	abs_track  dw 0x00
+    ; strings
+    message    db 'Hello World! Press any key to load the kernel ', 13, 10, 0
+    ; parameters
+    sectors_per_track  dw  18
+    heads_per_track    dw   2
+    bytes_per_sector   dw 512
+    drive_number       dw   0
+    ; variables
+    abs_sector dw 0x00
+    abs_head   dw 0x00
+    abs_track  dw 0x00
 
 ; routine for outputting 'si' register on the screen
 proj_e_boot_print:
-	mov ah, 0Eh      ; specify int 10h (teletype output)
+    mov ah, 0Eh      ; specify int 10h (teletype output)
 .printchar:
-	lodsb            ; load byte from si into al, increment si
-	cmp al, 0        ; is it the end of the string ?
-	je .done         ; yes => quit ; no => continue
-	int 10h          ; print the character
-	jmp .printchar
+    lodsb            ; load byte from si into al, increment si
+    cmp al, 0        ; is it the end of the string ?
+    je .done         ; yes => quit ; no => continue
+    int 10h          ; print the character
+    jmp .printchar
 .done:
-	ret
+    ret
 
 ; routine to reboot the machine
 proj_e_boot_reboot:
-	db 0x0ea         ; sending us to the end of the memory, to reboot
-	dw 0x0000
-	dw 0xffff
+    db 0x0ea         ; sending us to the end of the memory, to reboot
+    dw 0x0000
+    dw 0xffff
 
 ; routine to get a key press
 proj_e_boot_getkeypress:
-	mov ah, 0
-	int 16h          ; BIOS keyboard service
-	ret
+    mov ah, 0
+    int 16h          ; BIOS keyboard service
+    ret
 
 ; LBA to CHS
 ; input  : AX (LBA addr), sectors_per_track, heads_per_track
 ; output : abs_sector (CHS sector addr), abs_head (CHS head addr), abs_track (CHS track addr)
 proj_e_boot_lbachs:
-	xor dx, dx       ; dx=0
-	div WORD [sectors_per_track]
-	inc dl
-	mov BYTE [abs_sector], dl
+    xor dx, dx       ; dx=0
+    div WORD [sectors_per_track]
+    inc dl
+    mov BYTE [abs_sector], dl
 
-	xor dx, dx       ; dx=0
-	div WORD [heads_per_track]
-	mov BYTE [abs_head], dl
-	mov BYTE [abs_track], al
+    xor dx, dx       ; dx=0
+    div WORD [heads_per_track]
+    mov BYTE [abs_head], dl
+    mov BYTE [abs_track], al
 
-	ret
+    ret
 
 ; read sectors
 ; input  : CX (number of sectors to read), AX (LBA addr to start from)
 ; output : ES:BX (loaded sector addr:offset)
 proj_e_boot_readsectors:
-	proj_e_boot_sectorsmain:
-		mov di, 0x0005
-	proj_e_boot_sectorsloop:
-		push ax
-		push bx
-		push cx
-		call proj_e_boot_lbachs
+    proj_e_boot_sectorsmain:
+        mov di, 0x0005
+    proj_e_boot_sectorsloop:
+        push ax
+        push bx
+        push cx
+        call proj_e_boot_lbachs
 
-		mov ah, 0x02
-		mov al, 0x01
-		mov ch, BYTE [abs_track]
-		mov cl, BYTE [abs_sector]
-		mov dh, BYTE [abs_head]
-		mov dl, BYTE [drive_number]
+        mov ah, 0x02
+        mov al, 0x01
+        mov ch, BYTE [abs_track]
+        mov cl, BYTE [abs_sector]
+        mov dh, BYTE [abs_head]
+        mov dl, BYTE [drive_number]
 
-		int 13h
-		jnc proj_e_boot_sectorsdone
-		xor ax, ax        ; ax=0
-		int 13h
-		dec di
+        int 13h
+        jnc proj_e_boot_sectorsdone
+        xor ax, ax        ; ax=0
+        int 13h
+        dec di
 
-		pop cx
-		pop bx
-		pop ax
+        pop cx
+        pop bx
+        pop ax
 
-		jnz proj_e_boot_sectorsloop
-		int 18h
-	proj_e_boot_sectorsdone:
-		pop cx
-		pop bx
-		pop ax
+        jnz proj_e_boot_sectorsloop
+        int 18h
+    proj_e_boot_sectorsdone:
+        pop cx
+        pop bx
+        pop ax
 
-		add bx, WORD [bytes_per_sector]
-		inc ax
-		loop proj_e_boot_sectorsmain
-		ret
+        add bx, WORD [bytes_per_sector]
+        inc ax
+        loop proj_e_boot_sectorsmain
+        ret
 
 ; pad to 510 bytes (boot sector - 2)
 times 510-($-$$) db 0

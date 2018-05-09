2e75589c7cf010c86184051f4e27187cb9ea40e9
SuperFola <lexplt.dev@gmail.com>
2018-05-09 20:08:04 +0200

    fixing syntax in kernel.asm

--- kernel/kernel.asm
+++ kernel/kernel.asm
@@ -2,28 +2,28 @@ bits 16
 org 0
 
 start:
-	jmp main
+    jmp main
 
 data:
-	message db 'Kernel loaded', 13, 10, 0
+    message db 'Kernel loaded', 13, 10, 0
 
 main:
-	mov ax, cs
-	mov ds, ax
+    mov ax, cs
+    mov ds, ax
 
-	mov si, message
-	call print
-	; jump here
-	jmp $
+    mov si, message
+    call print
+    ; jump here
+    jmp $
 
 print:
-	lodsb
-	or al, al
-	jz printdone
-	mov ah, 0eh
-	int 10h
-	jmp print
+    lodsb
+    or al, al
+    jz printdone
+    mov ah, 0eh
+    int 10h
+    jmp print
 printdone:
-	ret
+    ret
 
 times 1024-($-$$) db 0
